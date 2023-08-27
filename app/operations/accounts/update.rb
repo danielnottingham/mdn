1fdf4c84bbc5bb53b674d23a1b33b2fadae87e32
Daniel Nottingham <54644738+danielnottingham@users.noreply.github.com>
2023-08-27 19:02:16 -0300

    ✨ feat: allow account update (#20)

---
+++ app/operations/accounts/update.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Accounts
+  class Update < Actor
+    input :id, type: String
+    input :attributes, type: Hash
+
+    output :account, type: Account
+
+    def call
+      self.account = find_account
+
+      fail!(error: :invalid_record) unless account.update(attributes)
+    end
+
+    def find_account
+      Accounts::Find.result(id: id).account
+    end
+  end
+end
