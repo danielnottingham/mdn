fb489cde5dd5b40ba71f2abef3b6371928d4e5d5
Daniel Nottingham <54644738+danielnottingham@users.noreply.github.com>
2023-09-03 18:44:40 -0300

    ✨ feat: allow account destroy (#21)

--- spec/components/accounts/list_component_spec.rb
+++ spec/components/accounts/list_component_spec.rb
@@ -3,14 +3,15 @@
 require "rails_helper"
 
 RSpec.describe Accounts::ListComponent, type: :component do
-  it "renders table accounts title, balance_cents and color" do
-    accounts = build_stubbed_list(:account, 3)
+  it "renders table accounts with title, balance_cents and color" do
+    accounts = create_list(:account, 3)
     rendered = render_inline(described_class.new(accounts: accounts))
 
     accounts.each do |account|
       expect(rendered.to_html).to have_text(account.title)
         .and have_text(account.balance_cents)
         .and have_text(account.color)
+        .and have_link(I18n.t("accounts.list_component.edit"), href: edit_account_path(account.id))
     end
   end
 end
