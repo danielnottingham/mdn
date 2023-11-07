07412a3e6bddd8a41edfd23352a6bc3e8aac64e5
Daniel Nottingham <danielnottingham.ti@gmail.com>
2023-11-07 19:38:27 -0300

    ♻ refactor: request tests

--- spec/policies/application_policy_spec.rb
+++ spec/policies/application_policy_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-require "action_policy/rspec"
 
 RSpec.describe ApplicationPolicy do
   # rubocop:disable RSpec/ExpectActual
--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -4,62 +4,89 @@ require "rails_helper"
 
 RSpec.describe "Categories" do
   describe "GET #index" do
-    context "when user is logged in" do
-      it "returns a successful response" do
-        sign_in
-        get categories_path
+    it "requires authentication" do
+      build_stubbed(:category)
+      get categories_path
 
-        expect(response).to have_http_status :ok
-      end
+      expect(response).to be_authenticated
     end
 
-    context "when user is not logged in" do
-      it "returns a redirect response" do
-        get categories_path
+    it "returns a successful response" do
+      sign_in
+      get categories_path
 
-        expect(response).to have_http_status :redirect
-      end
+      expect(response).to have_http_status :ok
     end
   end
 
   describe "GET #new" do
-    context "when user is logged in" do
-      it "returns a successful response" do
-        sign_in
-        get new_category_path
+    it "requires authentication" do
+      get new_category_path
 
-        expect(response).to have_http_status :ok
-      end
+      expect(response).to be_authenticated
     end
 
-    context "when user is not logged in" do
-      it "returns a redirect response" do
-        get new_category_path
+    it "returns a successful response" do
+      sign_in
+      get new_category_path
 
-        expect(response).to have_http_status :redirect
-      end
+      expect(response).to have_http_status :ok
     end
   end
 
   describe "GET #edit" do
-    context "when user is logged in" do
-      it "returns a successful response" do
-        user = create(:user)
-        sign_in(user)
-        category = create(:category, user: user)
-        get edit_category_path(category)
+    it "requires authentication" do
+      category = create(:category)
+      get edit_category_path(category)
 
-        expect(response).to have_http_status :ok
-      end
+      expect(response).to be_authenticated
     end
 
-    context "when user is not logged in" do
-      it "returns a redirect response" do
-        category = create(:category)
-        get edit_category_path(category)
+    it "authorizes category" do
+      sign_in
+      category = create(:category)
 
-        expect(response).to have_http_status :redirect
-      end
+      expect { get edit_category_path(category) }.to be_authorized_to(:update?, category)
+    end
+  end
+
+  describe "POST #create" do
+    it "requires authentication" do
+      post categories_path
+
+      expect(response).to be_authenticated
+    end
+  end
+
+  describe "PUT #update" do
+    it "authorizes category" do
+      sign_in
+      category = create(:category)
+
+      expect { put category_path(category.id) }.to be_authorized_to(:update?, category)
+    end
+
+    it "requires authentication" do
+      category = create(:category)
+      put category_path(category)
+
+      expect(response).to be_authenticated
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "authorizes category" do
+      sign_in
+      category = create(:category)
+
+      expect { delete category_path(category) }.to be_authorized_to(:destroy?, category)
+    end
+
+    it "requires authentication" do
+      category = create(:category)
+      delete category_path(category)
+
+      expect(response).to be_authenticated
     end
   end
 end
